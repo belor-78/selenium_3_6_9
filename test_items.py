fce20ad241ab45f25ec87625bf1f34ed40638b4e
belor-78 <belor-78@rambler.ru>
2019-12-24 21:23:02 +0200

    пятая попытка

--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ import pytest
 
 url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_language(browser):
+def test_basket_button(browser):
     browser.get(url)
-    browser.find_element_by_css_selector('#add_to_basket_form')
+    buttons = browser.find_elements_by_css_selector('#add_to_basket_form')
+    assert len(buttons) > 0, 'Кнопки нет'
